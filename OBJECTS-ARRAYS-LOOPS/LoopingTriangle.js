6968702494d21ccdd9e5ed035b9a8ec7baae4d03
Izabela <izabelac163@gmail.com>
2020-03-17 19:11:22 +0100

    Loops

--- OBJECTS-ARRAYS-LOOPS/LoopingTriangle.js
+++ OBJECTS-ARRAYS-LOOPS/LoopingTriangle.js
@@ -8,7 +8,7 @@ function createLoopingTriangle() {
     // how many times j to be true for each time of i ?
     for (j = 0; j + 2 < i; j++) {
       tempStars = tempStars + "* ";
-      console.log(`i + 2 is ${i + 2}: j is ${j} ::  ${tempStars}`);
+      console.log(`i is ${i + 2}: j is ${j} ::  ${tempStars}`);
     }
   }
 }
---
+++ OBJECTS-ARRAYS-LOOPS/loopExercise1.js
@@ -0,0 +1,21 @@
+let apple = { name: "apple", color: "red" };
+let banana = { name: "banana", color: "yellow" };
+let cherry = { name: "cherry", color: "red" };
+let guava = { name: "guava", color: "green" };
+let pineapple = { name: "pineapple", color: "red" };
+let grapes = { name: "grapes", color: "green" };
+
+var fruitList = [apple, banana, cherry, guava, pineapple, grapes];
+// print the name and color of the 3rd and 5th fruits
+// example: cherry is red and grapes is green
+
+console.log(fruitList[2].name, fruitList[2].color);
+console.log(fruitList[5].name, fruitList[5].color);
+
+//1-100 print every 2 number
+function everySecondNum() {
+  for (i = 0; i <= 100; i++) {
+    i = i + 2;
+    console.log(i);
+  }
+}
