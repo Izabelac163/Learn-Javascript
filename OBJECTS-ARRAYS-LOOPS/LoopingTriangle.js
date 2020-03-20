4853440220b66c35969c46e3e7b8782c675063c1
Izabela <izabelac163@gmail.com>
2020-03-20 20:59:45 +0100

    one step close

--- OBJECTS-ARRAYS-LOOPS/LoopingTriangle.js
+++ OBJECTS-ARRAYS-LOOPS/LoopingTriangle.js
@@ -4,11 +4,14 @@
 var tempStars = " ";
 var emptySpace = " ";
 function createLoopingTriangle() {
-  for (i = 4; i >= 1; i--) {
+  for (i = 0; i <= 4; i++) {
     // how many times j to be true for each time of i ?
     for (j = 0; j < i; j++) {
       emptySpace += " ";
-      for (k = 0; k < i; ) tempStars = tempStars + "*";
+
+      for (k = 0; k < i; k++) {
+        tempStars = tempStars + "*";
+      }
       // console.log(`i is ${i}: j is ${j} ::  ${tempStars}`);
     }
     console.log(tempStars);
