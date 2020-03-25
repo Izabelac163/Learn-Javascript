00637c9a2d64d0728db3bab0e85777b537ee1ce0
Izabela <izabelac163@gmail.com>
2020-03-25 21:49:34 +0100

    all tasks done

---
+++ OBJECTS-ARRAYS-LOOPS/areaOfCircle.js
@@ -0,0 +1,9 @@
+// 1. write a function that calculates the area of a circle - PI * R * R
+
+function findArea(Radius) {
+  var area = 3.14 * Radius * Radius;
+  return area;
+}
+
+var areaFound = findArea(6);
+console.log(areaFound);
